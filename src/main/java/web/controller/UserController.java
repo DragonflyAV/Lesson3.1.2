bef3497de9b3a17e92e8aae3effec2ca3d1cdccc
Sanya <sasha141412@gmail.com>
2023-01-24 21:32:37 +0300

    v1

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,70 @@
+package web.controller;
+
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.services.UserService;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+	private final UserService userService;
+
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping()
+	public String index(ModelMap model) {
+		model.addAttribute("users", userService.findAll());
+		return "pages/index";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") int id, ModelMap model) {
+		model.addAttribute("user", userService.findOne(id));
+		return "pages/show";
+	}
+
+	@GetMapping("/new")
+	public String newPerson(@ModelAttribute("user") User user) {
+		return "pages/new";
+	}
+
+	@PostMapping()
+	public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "pages/new";
+		}
+		userService.save(user);
+		return "redirect:/";
+	}
+
+	@GetMapping("/{id}/edit")
+	public String edit(ModelMap model, @PathVariable("id") int id) {
+		model.addAttribute("user", userService.findOne(id));
+		return "pages/edit";
+	}
+
+	@PatchMapping("/{id}")
+	public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+						 @PathVariable("id") int id) {
+		if (bindingResult.hasErrors()) {
+			return "pages/edit";
+		}
+		userService.update(id, user);
+		return "redirect:/";
+	}
+
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") int id) {
+		userService.delete(id);
+		return "redirect:/";
+	}
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,82 @@
+package web.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column (name = "name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @Column (name = "age")
+    @Min(value = 0, message = "Age should be greater than 0")
+    private int age;
+
+    @Column (name = "email")
+    @NotEmpty (message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
